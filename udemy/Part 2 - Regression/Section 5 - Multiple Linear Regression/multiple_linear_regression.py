f415e4e754df009b5c6d13e1a2d6d3826c91e019
binaykumar19 <binaykumar19@gmail.com>
2019-01-18 15:17:21 -0500

    Udemy : practice multiple linear regression

--- udemy/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ udemy/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -24,14 +24,6 @@ X = X[:, 1:]
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
-
 # Fitting Multiple Linear Regression to the Training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
@@ -39,3 +31,49 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+#Building the optimal model using Backward Elimination (Homework)
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50,1)).astype(int),values = X, axis = 1)
+X_opt = X[:,[0,1,2,3,4,5]]
+regressor_OLS = sm.OLS(endog=y,exog=X_opt)
+regressor_OLS = regressor_OLS.fit()
+regressor_OLS.summary()
+#removing 3rd column
+X_opt = X[:,[0,1,3,4,5]]
+regressor_OLS = sm.OLS(endog=y,exog=X_opt)
+regressor_OLS = regressor_OLS.fit()
+regressor_OLS.summary()
+#removing 2nd column
+X_opt = X[:,[0,3,4,5]]
+regressor_OLS = sm.OLS(endog=y,exog=X_opt)
+regressor_OLS = regressor_OLS.fit()
+regressor_OLS.summary()
+#removing 3rd column
+X_opt = X[:,[0,3,5]]
+regressor_OLS = sm.OLS(endog=y,exog=X_opt)
+regressor_OLS = regressor_OLS.fit()
+regressor_OLS.summary()
+#removing 3rd column
+X_opt = X[:,[0,3]]
+regressor_OLS = sm.OLS(endog=y,exog=X_opt)
+regressor_OLS = regressor_OLS.fit()
+regressor_OLS.summary()
+
+#Automatic Backward Elimination Algo 
+import statsmodels.formula.api as sm
+def backwardElimination(x, sl):
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+    regressor_OLS.summary()
+    return x
+ 
+SL = 0.05
+X_opt2 = X[:, [0, 1, 2, 3, 4, 5]]
+X_Modeled = backwardElimination(X_opt2, SL)
